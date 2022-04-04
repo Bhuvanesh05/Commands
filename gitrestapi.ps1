8eae1c5ee80518092b85e0158e8e653515b07b08
Bhuvanesh05 <kumarbhuvanesh625@gmail.com>
2022-04-04 14:40:14 +0530

    update

--- gitrestapi.ps1
+++ gitrestapi.ps1
@@ -1,4 +1,4 @@
-$pat = "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"
+$pat = "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"
 $header = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($pat)")) }
 $url = "https://api.github.com/repos/Bhuvanesh05/Commands/pulls?state=all"
 write-host "url : $url" 
@@ -13,9 +13,10 @@ for ($i = 0; $i -lt $response.Count; $i++) {
     
     if(($build1 -eq 'sfdx') -and ($build2 -eq 'master'))
     {
-        write-host "pull request ID:$build"
-        write-host "pull Number:$pullnum"
-        write-host "head branch:$build1"
-        write-host "base branch:$build2"
+        write-host "pull request ID:$build  pull Number:$pullnum  head branch:$build1  base branch:$build2"
+       # write-host "pull Number:$pullnum"
+      #  write-host "head branch:$build1"
+       # write-host "base branch:$build2"
     }    
+   
 }
