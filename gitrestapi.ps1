82ad77a9ced6f2fc545e45d99f7738f80f961658
Bhuvanesh05 <kumarbhuvanesh625@gmail.com>
2022-04-04 17:54:22 +0530

    update

--- gitrestapi.ps1
+++ gitrestapi.ps1
@@ -1,10 +1,11 @@
-$pat = "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"
+$pat = "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"
 $header = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($pat)")) }
 $url = "https://api.github.com/repos/Bhuvanesh05/Commands/pulls?state=all"
 write-host "url : $url" 
 $response = Invoke-RestMethod -Uri $url -Headers $header -Method Get
 
-#$build = $response[0].id
+$repname = $response[0].head.repo.name
+write-host "Repository Name:$repname"   
 for ($i = 0; $i -lt $response.Count; $i++) {
     $build = $response[$i].id
     $pullnum = $response[$i].number
@@ -13,10 +14,10 @@ for ($i = 0; $i -lt $response.Count; $i++) {
     $build3 = $response[$i].state
     if(($build1 -eq 'sfdx') -and ($build2 -eq 'master'))
     {
-        write-host "pull request ID:$build  
-               pull Number:$pullnum  
-               head branch:$build1  
-               base branch:$build2
+        write-host "  `r`n pull request ID:$build  `r`n
+               pull Number:$pullnum  `r`n
+               head branch(From):$build1  `r`n
+               base branch(To):$build2 `r`n
                pull request Status:$build3"
         #write-host "pull request Status:$build3"
       #  write-host "head branch:$build1"
